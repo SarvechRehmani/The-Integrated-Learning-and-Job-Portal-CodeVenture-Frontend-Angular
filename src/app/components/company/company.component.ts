f3c8de257154d4334888c23c4e58b6e0f0b4e242
Sarvech <sarwech99@gmail.com>
2025-08-12 13:58:38 +0500

    Now move on company panel dashboard.

--- src/app/components/company/company.component.ts
+++ src/app/components/company/company.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent {
+  sidebarOpen = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (!this.isMobile() && !this.sidebarOpen) {
+      this.sidebarOpen = true;
+    }
+  }
 
+  isMobile(): boolean {
+    return window.innerWidth < 768; // Tailwind's md breakpoint
+  }
+
+  ngOnInit() {
+    // Open sidebar by default on desktop
+    if (!this.isMobile()) {
+      this.sidebarOpen = true;
+    }
+  }
+  onSidebarToggle() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
--- src/app/components/company/sidebar-company/sidebar-company.component.ts
+++ src/app/components/company/sidebar-company/sidebar-company.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sidebar-company',
   templateUrl: './sidebar-company.component.html',
-  styleUrls: ['./sidebar-company.component.css']
+  styleUrls: ['./sidebar-company.component.css'],
 })
 export class SidebarCompanyComponent {
-  ngOnInit(): void {
-  }
-constructor(private login:LoginService){
+  @Input() isOpen = false;
+  @Output() isOpenChange = new EventEmitter<boolean>();
 
-}
-  public logout(){
+  sidebarLinks = [
+    { path: '/company/', label: 'Home', icon: 'home' },
+    { path: '/company/profile', label: 'Profile', icon: 'account_circle' },
+    { path: '/company/addjob', label: 'Add Job', icon: 'work' },
+    {
+      path: '/company/viewjobs',
+      label: 'Show Jobs',
+      icon: 'assignment_turned_in',
+    },
+    {
+      path: '/company/applications',
+      label: 'Show Applications',
+      icon: 'shield_person',
+    },
+  ];
+  user: any;
+  constructor(private login: LoginService) {}
+  ngOnInit(): void {
+    this.user = this.login.getUser();
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+    this.isOpenChange.emit(this.isOpen);
+  }
+
+  close() {
+    this.isOpen = false;
+    this.isOpenChange.emit(false);
+  }
+
+  public logout() {
     this.login.logout();
     window.location.reload();
   }
-
 }
